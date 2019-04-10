547c958bde243cdffcf3156e6aaf924a44e395db
arnob635 <arnob635@gmail.com>
2019-04-10 23:16:32 +0600

    UI

--- app/src/main/java/com/piperpay/pip_pay/Login.java
+++ app/src/main/java/com/piperpay/pip_pay/Login.java
@@ -1,4 +1,32 @@
 package com.piperpay.pip_pay;
 
-public class Login {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txt=(TextView) findViewById(R.id.create);
+
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent change = new Intent(Login.this,Signup.class);
+                startActivity(change);
+            }
+        });
+
+
+    }
+
+
 }
