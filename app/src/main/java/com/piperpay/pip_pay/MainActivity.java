8dc867d9dd4fa2bc1a967edac17891560029e839
arnob635 <arnob635@gmail.com>
2019-06-16 00:13:08 +0600

    Dashboard updated

--- app/src/main/java/com/piperpay/pip_pay/MainActivity.java
+++ app/src/main/java/com/piperpay/pip_pay/MainActivity.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +28,31 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.finish();
             }
         },splash_display_len);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Branch branch = Branch.getInstance();
+
+        // Branch init
+        branch.initSession(new Branch.BranchReferralInitListener() {
+            @Override
+            public void onInitFinished(JSONObject referringParams, BranchError error) {
+                if (error == null) {
+                    // params are the deep linked params associated with the link that the user clicked -> was re-directed to this app
+                    // params will be empty if no data found
+                    // ... insert custom logic here ...
+                    Log.i("BRANCH SDK", referringParams.toString());
+                } else {
+                    Log.i("BRANCH SDK", error.getMessage());
+                }
+            }
+        }, this.getIntent().getData(), this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        this.setIntent(intent);
     }
 }
